693f7007af529bc102d6ad1624ab835e51612344
student <student@James-MacBook-Pro.local>
2020-05-19 02:53:31 +0100

    Updates

---
+++ precache-manifest.e8b516d60ee46b4fac23c09d94781a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa1d05bd021a1694f9a8adf41119f17",
+    "url": "/buy-a-brew/index.html"
+  },
+  {
+    "revision": "2ebf1ec51f79c5986ed9",
+    "url": "/buy-a-brew/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0007172069e9db5a5856",
+    "url": "/buy-a-brew/static/js/2.a517b147.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/buy-a-brew/static/js/2.a517b147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebf1ec51f79c5986ed9",
+    "url": "/buy-a-brew/static/js/main.950d94a4.chunk.js"
+  },
+  {
+    "revision": "4d4449e889eda9e335aa",
+    "url": "/buy-a-brew/static/js/runtime-main.471c7a0c.js"
+  }
+]);
